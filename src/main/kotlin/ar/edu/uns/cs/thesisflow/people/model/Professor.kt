d05cd7479abec1f260655e6a105ee266c29c177b
Ignacio Dotta <ij.dotta@gmail.com>
2025-09-10 20:11:14 -0300

    impl Professor.kt

---
+++ src/main/kotlin/ar/edu/uns/cs/thesisflow/people/model/Professor.kt
@@ -0,0 +1,25 @@
+package ar.edu.uns.cs.thesisflow.people.model
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Index
+import jakarta.persistence.Table
+import java.util.UUID
+
+@Entity
+@Table(indexes = [Index(name = "uuid", columnList = "uuid")])
+class Professor(
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long,
+    @Column(nullable = false, unique = true, updatable = false)
+    var uuid: UUID = UUID.randomUUID(),
+    @Column(nullable = false)
+    var name: String,
+    @Column(nullable = false)
+    var lastname: String,
+    @Column(nullable = false, unique = true)
+    var email: String,
+)
