73f3f8206adbbea67dacccd23029f57d22763410
Ignacio Dotta <ij.dotta@gmail.com>
2025-09-10 19:30:01 -0300

    initial commit

---
+++ src/main/kotlin/ar/edu/uns/cs/thesisflow/ThesisFlowApplication.kt
@@ -0,0 +1,11 @@
+package ar.edu.uns.cs.thesisflow
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ThesisFlowApplication
+
+fun main(args: Array<String>) {
+    runApplication<ThesisFlowApplication>(*args)
+}
---
+++ src/test/kotlin/ar/edu/uns/cs/thesisflow/ThesisFlowApplicationTests.kt
@@ -0,0 +1,13 @@
+package ar.edu.uns.cs.thesisflow
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ThesisFlowApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
