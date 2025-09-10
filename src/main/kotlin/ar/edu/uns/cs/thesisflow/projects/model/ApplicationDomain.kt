1137bd25bc83b8097e727a946193dc3ca459d370
Ignacio Dotta <ij.dotta@gmail.com>
2025-09-10 20:21:37 -0300

    impl ApplicationDomain.kt

---
+++ src/main/kotlin/ar/edu/uns/cs/thesisflow/projects/model/ApplicationDomain.kt
@@ -0,0 +1,23 @@
+package ar.edu.uns.cs.thesisflow.projects.model
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Index
+import jakarta.persistence.Table
+import java.util.UUID
+
+@Entity
+@Table(indexes = [Index(name = "uuid", columnList = "uuid")])
+class ApplicationDomain(
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long,
+    @Column(nullable = false, unique = true, updatable = false)
+    var uuid: UUID = UUID.randomUUID(),
+    @Column(nullable = false, unique = true)
+    var name: String,
+    @Column(nullable = false)
+    var description: String
+)
