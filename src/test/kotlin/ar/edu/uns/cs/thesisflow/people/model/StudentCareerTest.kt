4ffc456fc4099733dc11e55852320abf89b2f106
Ignacio Dotta <ij.dotta@gmail.com>
2025-09-13 22:23:48 -0300

    refactor - move files

--- src/test/kotlin/ar/edu/uns/cs/thesisflow/people/model/StudentCareerTest.kt
+++
@@ -1,9 +0,0 @@
-package ar.edu.uns.cs.thesisflow.people.model
-
-import org.junit.jupiter.api.Assertions.*
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
-
-@DataJpaTest
-class StudentCareerTest {
-
-}
