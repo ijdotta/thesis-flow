2f7f471784629c8a7690a35ae1605ef30569b806
Ignacio Dotta <ij.dotta@gmail.com>
2025-09-14 20:21:42 -0300

    professor

---
+++ src/main/kotlin/ar/edu/uns/cs/thesisflow/people/api/ProfessorController.kt
@@ -0,0 +1,35 @@
+package ar.edu.uns.cs.thesisflow.people.api
+
+import ar.edu.uns.cs.thesisflow.people.dto.ProfessorDTO
+import ar.edu.uns.cs.thesisflow.people.service.ProfessorService
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/professors")
+class ProfessorController(
+    private val professorService: ProfessorService,
+) {
+    @GetMapping
+    fun findAll() = ResponseEntity.ok(professorService.findAll())
+
+    @GetMapping("/{publicId}")
+    fun findById(@PathVariable publicId: String) = ResponseEntity
+        .ok(professorService.findByPublicId(publicId))
+
+    @PostMapping
+    fun create(@RequestBody professor: ProfessorDTO) = ResponseEntity
+        .ok(professorService.create(professor))
+
+    @PutMapping("/{publicId}")
+    fun update(@PathVariable publicId: String, @RequestBody professorDTO: ProfessorDTO) = with(professorDTO) {
+        val professorWithId = professorDTO.copy(publicId = publicId)
+        ResponseEntity.ok(professorService.update(professorWithId))
+    }
+}
---
+++ src/main/kotlin/ar/edu/uns/cs/thesisflow/people/service/ProfessorService.kt
@@ -0,0 +1,70 @@
+package ar.edu.uns.cs.thesisflow.people.service
+
+import ar.edu.uns.cs.thesisflow.people.dto.ProfessorDTO
+import ar.edu.uns.cs.thesisflow.people.dto.toDTO
+import ar.edu.uns.cs.thesisflow.people.persistance.repository.PersonRepository
+import ar.edu.uns.cs.thesisflow.people.persistance.repository.ProfessorRepository
+import org.springframework.stereotype.Service
+import java.util.UUID
+
+@Service
+class ProfessorService(
+    private val professorRepository: ProfessorRepository,
+    private val personRepository: PersonRepository
+) {
+    companion object {
+        val VALID_EMAIL_DOMAINS = listOf(
+            "@cs.uns.edu.ar",
+            "@uns.edu.ar"
+        )
+    }
+
+    fun findAll() = professorRepository.findAll().map { it.toDTO() }
+    fun findByPublicId(publicId: String) = findEntityByPublicId(publicId).toDTO()
+
+    private fun findEntityByPublicId(publicId: String?) =
+        publicId?.let { professorRepository.findByPublicId(UUID.fromString(it)) }
+            ?: throw IllegalArgumentException("No professor found for $publicId")
+
+    fun create(professorDTO: ProfessorDTO): ProfessorDTO {
+        validate(professorDTO)
+        val person = professorDTO.getPerson()
+        val professor = professorDTO.toEntity(person)
+        return professorRepository.save(professor).toDTO()
+    }
+
+    private fun validate(professorDTO: ProfessorDTO) {
+        professorDTO.personPublicId?.let { checkNotAssociated(UUID.fromString(it)) }
+        professorDTO.email?.let { validateEmail(it) }
+    }
+
+    private fun ProfessorDTO.getPerson() = personPublicId?.let {
+        personRepository.findByPublicId(UUID.fromString(it))
+    } ?: throw IllegalArgumentException("No person found for $personPublicId")
+
+    private fun checkNotAssociated(personPublicId: UUID) {
+        if (professorRepository.existsByPersonPublicId(personPublicId)) {
+            throw IllegalArgumentException("Person $personPublicId is associated to other professor.")
+        }
+    }
+
+    private fun validateEmail(email: String?) {
+        if (email.isNullOrBlank()) {
+            throw IllegalArgumentException("Email cannot be null or blank.")
+        }
+        if (VALID_EMAIL_DOMAINS.none { email.endsWith(it) }) {
+            throw IllegalArgumentException("Email must end with '$VALID_EMAIL_DOMAINS'")
+        }
+    }
+
+    fun update(professorDTO: ProfessorDTO): ProfessorDTO {
+        validate(professorDTO)
+        val professor = findEntityByPublicId(professorDTO.publicId)
+        professorDTO.personPublicId?.let {
+            val person = professorDTO.getPerson()
+            professor.person = person
+        }
+        professorDTO.update(professor)
+        return professorRepository.save(professor).toDTO()
+    }
+}
