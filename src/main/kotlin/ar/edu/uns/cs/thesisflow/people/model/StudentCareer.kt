d969485f5f213f8bad7600e93b34bb8ae2e46ca6
Ignacio Dotta <ij.dotta@gmail.com>
2025-09-13 21:42:34 -0300

    student-career relation

--- src/main/kotlin/ar/edu/uns/cs/thesisflow/people/model/StudentCareer.kt
+++ src/main/kotlin/ar/edu/uns/cs/thesisflow/people/model/StudentCareer.kt
@@ -1,27 +1,24 @@
 package ar.edu.uns.cs.thesisflow.people.model
 
-import jakarta.persistence.Column
+import ar.edu.uns.cs.thesisflow.catalog.model.Career
 import jakarta.persistence.Entity
 import jakarta.persistence.FetchType
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
-import jakarta.persistence.Index
+import jakarta.persistence.JoinColumn
 import jakarta.persistence.ManyToOne
-import jakarta.persistence.Table
 import java.util.UUID
 
 @Entity
-@Table(indexes = [Index(name = "uuid", columnList = "uuid")])
-class Student(
+class StudentCareer(
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Long,
-    @Column(name = "public_id", nullable = false, unique = true, updatable = false)
     var publicId: UUID = UUID.randomUUID(),
     @ManyToOne(fetch = FetchType.LAZY)
-    var person: Person,
-    @Column(nullable = false, unique = true)
-    var studentId: String,
-    @Column(nullable = false, unique = true)
-    var email: String,
+    @JoinColumn(name = "student_id", nullable = false)
+    var student: Student,
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "career_id", nullable = false)
+    var career: Career,
 )
