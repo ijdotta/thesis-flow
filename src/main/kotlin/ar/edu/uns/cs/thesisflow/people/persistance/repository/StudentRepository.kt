d1fe9ce54980864bea2097ec183fc00e32e2216a
Ignacio Dotta <ij.dotta@gmail.com>
2025-09-14 19:39:20 -0300

    update contract

--- src/main/kotlin/ar/edu/uns/cs/thesisflow/people/persistance/repository/StudentRepository.kt
+++ src/main/kotlin/ar/edu/uns/cs/thesisflow/people/persistance/repository/StudentRepository.kt
@@ -1,9 +1,11 @@
 package ar.edu.uns.cs.thesisflow.people.persistance.repository
 
+import ar.edu.uns.cs.thesisflow.people.persistance.entity.Person
 import ar.edu.uns.cs.thesisflow.people.persistance.entity.Student
 import org.springframework.data.jpa.repository.JpaRepository
 import java.util.UUID
 
 interface StudentRepository: JpaRepository<Student, Long> {
     fun findByPublicId(publicId: UUID): Student?
+    fun findFirstByPerson(person: Person): Student?
 }
--- src/main/kotlin/ar/edu/uns/cs/thesisflow/people/service/StudentService.kt
+++ src/main/kotlin/ar/edu/uns/cs/thesisflow/people/service/StudentService.kt
@@ -1,17 +1,17 @@
 package ar.edu.uns.cs.thesisflow.people.service
 
-import ar.edu.uns.cs.thesisflow.people.dto.PersonDTO
 import ar.edu.uns.cs.thesisflow.people.dto.StudentDTO
 import ar.edu.uns.cs.thesisflow.people.dto.toDTO
 import ar.edu.uns.cs.thesisflow.people.persistance.entity.Person
+import ar.edu.uns.cs.thesisflow.people.persistance.repository.PersonRepository
 import ar.edu.uns.cs.thesisflow.people.persistance.repository.StudentRepository
 import org.springframework.stereotype.Service
-import java.util.UUID
+import java.util.*
 
 @Service
 class StudentService(
     private val studentRepository: StudentRepository,
-    private val personService: PersonService
+    private val personRepository: PersonRepository,
 ) {
     fun findAll() = studentRepository.findAll().map { it.toDTO() }
 
@@ -21,23 +21,23 @@ class StudentService(
         ?: throw IllegalArgumentException("No student found for publicId: $publicId")
 
     fun create(studentDTO: StudentDTO): StudentDTO {
-        val person = studentDTO.getOrCreatePerson()
+        val person = studentDTO.getPerson()
+        checkPersonNotAssociated(person)
         val student = studentDTO.toEntity(person)
         return studentRepository.save(student).toDTO()
     }
 
-    private fun StudentDTO.getOrCreatePerson() = Person(getPersonId())
-
-    private fun StudentDTO.getPersonId() = if (personPublicId != null) {
-            personService.findByPublicId(personPublicId).id!!
-        } else {
-            val personDTO = PersonDTO(name = name, lastname = lastname)
-            personService.create(personDTO).id!!
+    private fun checkPersonNotAssociated(person: Person) {
+        studentRepository.findFirstByPerson(person)?.let {
+            throw IllegalArgumentException("Person ${person.publicId} already associated to other student")
         }
+    }
+
+    private fun StudentDTO.getPerson() =
+        personPublicId?.let { personRepository.findByPublicId(UUID.fromString(it)) }
+            ?: throw IllegalArgumentException("No person found for publicId: $personPublicId")
 
     fun update(studentDTO: StudentDTO): StudentDTO {
-        val personDTO = PersonDTO(name = studentDTO.name, lastname = studentDTO.lastname)
-        personService.update(personDTO)
         val student = findEntityByPublicId(UUID.fromString(studentDTO.publicId!!))
         studentDTO.update(student)
         return studentRepository.save(student).toDTO()
