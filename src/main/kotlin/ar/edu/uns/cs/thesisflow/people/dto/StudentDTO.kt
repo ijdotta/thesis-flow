b83f5cd4067b4c5463f30c2b44bd5c4963815263
Ignacio Dotta <ij.dotta@gmail.com>
2025-09-28 18:28:54 -0300

    update dtos

--- src/main/kotlin/ar/edu/uns/cs/thesisflow/people/dto/StudentDTO.kt
+++ src/main/kotlin/ar/edu/uns/cs/thesisflow/people/dto/StudentDTO.kt
@@ -5,11 +5,13 @@ import ar.edu.uns.cs.thesisflow.people.persistance.entity.Person
 import ar.edu.uns.cs.thesisflow.people.persistance.entity.Student
 
 data class StudentDTO(
-    val publicId : String?,
-    val personPublicId : String?,
-    val person: PersonDTO?,
-    val studentId: String?,
-    val email: String?,
+    val publicId : String? = null,
+    val personPublicId : String? = null,
+    val person: PersonDTO? = null,
+    val name: String? = null,
+    val lastname: String? = null,
+    val studentId: String? = null,
+    val email: String? = null,
     val careers: List<CareerDTO> = emptyList(),
 ) {
     fun toEntity(person: Person) = Student(studentId = studentId!!, person = person, email = email!!)
