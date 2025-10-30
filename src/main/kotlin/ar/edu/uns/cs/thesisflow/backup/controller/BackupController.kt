fa231f0efa9c34f73991e44002d19efc64838ccf
Ignacio Dotta <67470951+ijdotta@users.noreply.github.com>
2025-10-30 20:39:03 -0300

    remove "/api" prefix (#45)

--- src/main/kotlin/ar/edu/uns/cs/thesisflow/backup/controller/BackupController.kt
+++ src/main/kotlin/ar/edu/uns/cs/thesisflow/backup/controller/BackupController.kt
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/api/backup")
+@RequestMapping("/backup")
 class BackupController(private val backupService: BackupService) {
 
     @GetMapping("/create")
